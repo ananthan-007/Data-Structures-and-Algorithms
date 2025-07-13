62eb7f38245883e4a340f3a18b1fd57f3ef6f295
Ananthakrishnan K V <ananthakrishnanvenu007@gmail.com>
2025-07-13 21:08:08 +0530

    Add files via upload

---
+++ Searching/Linear_search.c
@@ -0,0 +1,38 @@
+/*******************************************************
+ * --------------------------------------------------- *
+ *  Project   : Linear_search                          *
+ *  Author    : Ananthakrishnan K V                    *
+ *  Version   : v1.0                                   *
+ *  Language  : C                                      *
+ *  Created   : 13-07-2025                             *
+ *  Updated   : 13-07-2025                             *
+ *  Purpose   : C code to linearly search x in arr[].  *
+ * --------------------------------------------------- *
+ *            Let code flow like poetry! 🚀            *
+ *******************************************************/
+
+
+#include <stdio.h>
+
+int search(int arr[], int numOfElements, int x)
+{
+    for (int i = 0; i < numOfElements; i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
+}
+
+// Main function to test the search function
+int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x = 10;
+    int numOfElements = sizeof(arr) / sizeof(arr[0]);
+
+    // Function call
+    int result = search(arr, numOfElements, x);
+    (result == -1)
+        ? printf("Element is not present in array")
+        : printf("Element is present at index %d", result);
+    return 0;
+}
