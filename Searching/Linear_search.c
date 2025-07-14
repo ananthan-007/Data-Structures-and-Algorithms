95561bff4b3caf06b6a5b70b1ce2f2611c2fef1b
Ananthakrishnan K V <ananthakrishnanvenu007@gmail.com>
2025-07-14 21:22:56 +0530

    Update Linear_search.c

--- Searching/Linear_search.c
+++ Searching/Linear_search.c
@@ -2,7 +2,7 @@
  * --------------------------------------------------- *
  *  Project   : Linear_search                          *
  *  Author    : Ananthakrishnan K V                    *
- *  Version   : v1.0                                   *
+ *  Version   : 1.0                                   *
  *  Language  : C                                      *
  *  Created   : 13-07-2025                             *
  *  Updated   : 13-07-2025                             *
